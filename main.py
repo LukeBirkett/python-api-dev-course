38d1a3c28548a1933b39c5b5bfda4ea87db76b0b
Luke Birkett <contactbirkett@gmail.com>
2023-08-10 16:44:49 +0200

    timestamp

--- main.py
+++ main.py
@@ -62,3 +62,6 @@ def delete_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} not found")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# 2:10:31
