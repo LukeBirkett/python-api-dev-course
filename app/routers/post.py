39387af2b9ddbaeaa72e7a1e51009ebf86a40f7a
Luke Birkett <contactbirkett@gmail.com>
2023-08-29 20:14:13 +0100

    progress update

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,12 +10,19 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+):
     posts = db.query(models.Post).all()
     return posts
 
 @router.get("/{id}", response_model=schemas.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(
+    id: int, 
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -26,17 +33,21 @@ def get_post(id: int, db: Session = Depends(get_db)):
 def create_posts(
     post: schemas.PostCreate, 
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user)
+    current_user: int = Depends(oauth2.get_current_user)
 ):
+    print(current_user.email)
     new_post = models.Post(**post.dict())
-    print(new_post)
     db.add(new_post)
-    db.commit()
+    db.commit() 
     db.refresh(new_post)
     return new_post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(
+    id: int, 
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+ ):
     delete = db.query(models.Post).filter(models.Post.id == id)
 
     if delete.first() == None:
@@ -48,7 +59,12 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.PostResponse)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(
+    id: int, 
+    post: schemas.PostCreate, 
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+):
     updating_query = db.query(models.Post).filter(models.Post.id == id)
 
     if updating_query.first() == None:
