ae0fd03020d6e571e78ff9d72815b89e90bec170
Luke Birkett <contactbirkett@gmail.com>
2023-09-04 14:22:02 +0100

    tieing create post to user_id

--- app/routers/post.py
+++ app/routers/post.py
@@ -34,9 +34,8 @@ def create_posts(
     post: schemas.PostCreate, 
     db: Session = Depends(get_db),
     current_user: int = Depends(oauth2.get_current_user)
-):
-    print(current_user.email)
-    new_post = models.Post(**post.dict())
+): 
+    new_post = models.Post(user_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit() 
     db.refresh(new_post)
